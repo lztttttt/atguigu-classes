73c3f57ceb7e7365ddbd8df78fe2c7cb601f031b
1003 <1103581678@qq.com>
2021-11-04 00:16:20 +0800

    20211104_1

--- spark-core/src/main/java/com/atguigu/bigdata/spark/core/wc/Spark01_WordCount.scala
+++ spark-core/src/main/java/com/atguigu/bigdata/spark/core/wc/Spark01_WordCount.scala
@@ -1,11 +1,22 @@
 package com.atguigu.bigdata.spark.core.wc
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Spark01_WordCount {
 
   def main(args: Array[String]): Unit = {
     val sparConf = new SparkConf().setMaster("local").setAppName("WordCount")
-    val sc = new SparkContext()
+    val sc = new SparkContext(sparConf)
+    val lines: RDD[String] = sc.textFile(path = "datas")
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+    val wordGroup: RDD[(String, Iterable[String])] = words.groupBy(word=>word)
+    val wordToCount = wordGroup.map{
+      case(word,list) => {
+        (word, list.size)
+      }
+    }
+    val res: Array[(String, Int)] = wordToCount.collect()
+    res.foreach(println)
     sc.stop()
   }
 }
